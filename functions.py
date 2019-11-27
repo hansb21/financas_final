decee7cadde805c0c73580591bfdb51ed5654a68
hansb21 <hans.buss@mailfence.com>
2019-11-27 09:43:37 -0300

    Arrumado uma grande parte do gráfico

--- functions.py
+++ functions.py
@@ -1,16 +1,32 @@
 from alpha_vantage.techindicators import TechIndicators
 import matplotlib.pyplot as plt
 
-def get_sma(symbol, interval, time_period):
-    ti = TechIndicators(key='NARUTO', output_format='pandas')
-    data, metadata = ti.get_sma(symbol=symbol, interval=interval, time_period=time_period, series_type='close')
-    return data, metadata
+def get_sma(symbol):
+    ti = TechIndicators(key='SAKEHDUIAJSIJAISK', output_format='pandas')
+    data7, metadata = ti.get_sma(symbol=symbol, interval='60min', time_period=7, series_type='close')
+    data21, metadata = ti.get_sma(symbol=symbol, interval='60min', time_period=21, series_type='close')
+    return data7, data21
 
-def plot(sma7, sma21):
+def plot(acao):
     #color = 'r' if (sma7 < 0).all() else 'g'
-    plt.plot(sma7, color='g')
-    plt.plot(sma21, color='r')
+
+    sma7, sma21 = get_sma(acao)
+    
+    figura, eixo = plt.subplots()
+    
+    plt.subplots_adjust(bottom=0.2)
+    plt.plot(sma7, color='g', alpha=0.75, label='Média Móvel - 7')
+    plt.plot(sma21, color='r', alpha=0.75, label='Média Móvel - 21')
+    eixo.set(xlabel='Tempo', ylabel='Preço', title="{} Stocks 60min".format(acao))
+    
+    #Arruma a data
+    eixo.xaxis_date()
+    
+    #Gira os labels do gráfico para ficar menos poluido
+    for label in eixo.get_xticklabels():
+            label.set_rotation(20)
+    
+    #Mostra os labels
+    plt.legend()
+    
     plt.show()
-sma1, smax = get_sma('MSFT', '60min', 7)
-sma2, smay = get_sma('MSFT', '60min', 21)
-plot(sma1, sma2)
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from functions import *
 
+acoes_Validas = ['MSFT', 'GOGL', 'AMZN', 'TSLA']
+
+#TODO
+# Adicionar uma interface com 4 botões para selecionar as ações validas
+acao = 'AMZN'
+
+if acao not in acoes_Validas:
+    print('Essa ação não é valida. ') #A principio é só pra testes
+
+
+plot(acao)
