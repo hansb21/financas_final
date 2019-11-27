eb3034ce00fe0e55cf43c6b4ddd33600c76c55cb
hansb21 <hans.buss@mailfence.com>
2019-11-26 22:16:03 -0300

    Trocado Daily por 60min

--- functions.py
+++ functions.py
@@ -7,9 +7,10 @@ def get_sma(symbol, interval, time_period):
     return data, metadata
 
 def plot(sma7, sma21):
-    plt.plot(sma7, color='b')
+    #color = 'r' if (sma7 < 0).all() else 'g'
+    plt.plot(sma7, color='g')
     plt.plot(sma21, color='r')
     plt.show()
-sma1, smax = get_sma('MSFT', 'weekly', 7)
-sma2, smay = get_sma('MSFT', 'weekly', 21)
+sma1, smax = get_sma('MSFT', '60min', 7)
+sma2, smay = get_sma('MSFT', '60min', 21)
 plot(sma1, sma2)
